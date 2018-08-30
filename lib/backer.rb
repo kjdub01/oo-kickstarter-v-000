31788cc875499477b2533f4da7e29a30d7acf0be
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-08-30 04:10:58 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,10 +1,13 @@
 class Backer
-  attr_accessor :name, :back_project
+  attr_accessor :name, :backed_project
   
   def initialize(name)
     @name = name 
     @backed_projects = []
   end
   
+  def back_projects(project)
+    
+  end
   
 end
